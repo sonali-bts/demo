3191be3f2e5a69a58a1458cf220eaf728b62a70a
user <sjadhav@micron.com>
2020-09-04 16:23:19 +0530

    rebase

--- demogit.py
+++ demogit.py
@@ -1,7 +1,11 @@
 print('hello local master')
 print('hello global master')
 print('rebase exmaple to handle conflict')
+<<<<<<< HEAD
 print('rebase exmaple by git 2  ')
 
+=======
+print('rebase exmaple to git 1')
+>>>>>>> 847e359...  rebase example
 
 
