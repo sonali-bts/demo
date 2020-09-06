69f98018f91a2a6db51fd504d17853debf4ae6f6
user <sjadhav@micron.com>
2020-09-06 14:02:38 +0530

    ed

--- demogit.py
+++ demogit.py
@@ -1,8 +1,12 @@
 print('hello local master')
 print('rebase example by user 1')
+<<<<<<< HEAD
 print('rebase -commit AA')
 print('rebase -commit AB')
 
+=======
+print('rebase -commit A')
+>>>>>>> a43b780... commit A
 
 
 
